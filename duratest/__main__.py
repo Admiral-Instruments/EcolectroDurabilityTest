1b33e4255129a3981c2c9999213cfd224555df15
Coltor Leichtman <coltorleichtman@gmail.com>
2021-02-08 17:14:49 -0700

    async functions added

--- duratest/__main__.py
+++ duratest/__main__.py
@@ -1,4 +1,10 @@
 from experiment import Experiment
 
-if __name__ == "__main__":
+import asyncio
+
+async def main():
     test = Experiment()
+    await test.run_experiment()
+
+if __name__ == "__main__":
+    asyncio.run(main())
--- duratest/experiment.py
+++ duratest/experiment.py
@@ -1,8 +1,10 @@
 import json
+import asyncio
 
 from bk_operator import BKOperator
 from pump_controller import PumpController
 from temperature_controller import TemperatureController
+
 
 class Experiment:
     def __init__(self):
@@ -13,9 +15,22 @@ class Experiment:
         self.duration = data["duration"]
         self.save_path = data["save-path"]
 
-        self.bk_operator = self.__get_new_BK__(data["Power-Supply-options"])
-        self.pump_controller = self.__get_new_PumpController__(data["Pump-Controller-options"])
-        self.temp_controller = self.__get_new_TemperatureController(data["Temperature-Controller-options"])
+        self.bk_options = data["Power-Supply-options"]
+        self.pump_options = data["Pump-Controller-options"]
+        self.temperature_options = data["Temperature-Controller-options"]
+
+        self.bk_operator = self.__get_new_BK__(self.bk_options)
+        self.pump_controller = self.__get_new_PumpController__(self.pump_options)
+        self.temp_controller = self.__get_new_TemperatureController(self.temperature_options)
+
+    async def run_experiment(self):
+        while self.duration > 0:
+            self.__get_readings__()
+            self.duration -= self.sampling_rate
+            await asyncio.sleep(self.sampling_rate)
+
+    def __get_readings__(self) -> bool:
+        return True
 
     def __get_new_BK__(self, bk_dict: dict) -> BKOperator:
         try:
